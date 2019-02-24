a98c02e7e53cf9c8f20256e9590ad8f5fe977762
VikramBaraiya <contactbaraiya@gmail.com>
2019-02-23 22:05:30 -0500

    functionality of adding user to database to keep in mind:- cant perform database operation on main thread by default to forcefully perform on main thread allowmainthreadquereis in the database builder object

--- app/src/main/java/e/y/myapplication/AddUserFragment.java
+++ app/src/main/java/e/y/myapplication/AddUserFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
@@ -13,7 +16,8 @@ import android.view.ViewGroup;
  */
 public class AddUserFragment extends Fragment {
 
-
+    Button save;
+    EditText id,name,email;
     public AddUserFragment() {
         // Required empty public constructor
     }
@@ -23,7 +27,33 @@ public class AddUserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_user, container, false);
+        View view;
+        view= inflater.inflate(R.layout.fragment_add_user, container, false);
+       //ave=view.findViewById(R.id.)
+         id=view.findViewById(R.id.id);
+         name=view.findViewById(R.id.Name);
+         email=view.findViewById(R.id.Email);
+         save=view.findViewById(R.id.save);
+
+         save.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 int userid=Integer.parseInt(id.getText().toString());
+                 String username=name.getText().toString();
+                 String useremail=email.getText().toString();
+                 User user=new User();
+                 user.setId(userid);
+                 user.setName(username);
+                 user.setEmail(useremail);
+                 MainActivity.user.User_Dao().insertUser(user);
+                 Toast.makeText(getActivity(),"USer Added Successfully",Toast.LENGTH_SHORT).show();
+                 id.setText("");
+                 name.setText("");
+                 email.setText("");
+
+             }
+         });
+         return view;
     }
 
 }
--- app/src/main/java/e/y/myapplication/MainActivity.java
+++ app/src/main/java/e/y/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package e.y.myapplication;
 
+import android.arch.persistence.room.Room;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -10,11 +11,14 @@ public class MainActivity extends AppCompatActivity {
 
     public static FragmentManager fm;
     //FragmentTransaction ft;
+    public static UserRoomDataBase user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fm=getSupportFragmentManager();
+        user= Room.databaseBuilder(getApplicationContext(),UserRoomDataBase.class,"UserDB").allowMainThreadQueries().build();
+
         if(findViewById(R.id.Fragment_Container)!=null)
         {
             if(savedInstanceState!=null)
